60d70cc8f3366497619a4226b65982d0e0b14cf9
Seyfullah Tıkıç <stikic@gmail.com>
2024-04-28 19:52:48 +0300

    Ek geliştirmeler

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # kelimem_dogru_mu
 
 https://davidwalsh.name/browser-camera
+
+C:\Users\your_windows_username\.aws
+contains
+
+config file as:
+
+[default]
+region = eu-west-2
+
+credential file as:
+
+[default]
+aws_access_key_id = your_aws_access_key_id
+aws_secret_access_key = your_aws_secret_access_key
+
+[your_user_name]
+# This key identifies your AWS account.
+aws_access_key_id = your_aws_access_key_id
+# Treat this secret key like a password. Never share it or store it in source
+# control. If your secret key is ever disclosed, immediately use IAM to delete
+# the key pair and create a new one.
+aws_secret_access_key = your_aws_secret_access_key
+
+
+Example call:
+python client.py detect_text s3://detect-text-bucket/basit.jpg
---
+++ aws/lambda/lambda_handler.py
@@ -0,0 +1,91 @@
+ 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Purpose
+An AWS lambda function that analyzes documents with Amazon Textract.
+"""
+import json
+import base64
+import logging
+import boto3
+import os
+
+from botocore.exceptions import ClientError
+
+# Set up logging.
+logger = logging.getLogger(__name__)
+logger = logging.getLogger()
+logger.setLevel("INFO")
+# Get the boto3 client.
+textract_client = boto3.client('textract')
+
+
+def lambda_handler(event, context):
+    """
+    Lambda handler function
+    param: event: The event object for the Lambda function.
+    param: context: The context object for the lambda function.
+    return: The list of Block objects recognized in the document
+    passed in the event object.
+    """
+    try:
+        # Determine document source.
+        if 'image' in event:
+            # Decode the image
+            image_bytes = event['image'].encode('utf-8')
+            img_b64decoded = base64.b64decode(image_bytes)
+            image = {'Bytes': img_b64decoded}
+        elif 'body' in event:
+            # Decode the image
+            new_string = ''.join(event['body'].split('\r\n'))
+            new_dict = json.loads(new_string)
+            image_bytes = new_dict['image'].encode('utf-8')
+            img_b64decoded = base64.b64decode(image_bytes)
+            image = {'Bytes': img_b64decoded}
+        elif 'S3Object' in event:
+            image = {'S3Object':
+                     {'Bucket':  event['S3Object']['Bucket'],
+                      'Name': event['S3Object']['Name']}
+                     }
+        else:
+            raise ValueError('Invalid source. Only image base 64 encoded image bytes or S3Object are supported.')
+
+        # Analyze the document.
+        response = textract_client.detect_document_text(Document=image)
+
+        # Get the Blocks
+        blocks = response['Blocks'][1]["Text"]
+
+        lambda_response = {
+            "statusCode": 200,
+            "body": json.dumps(blocks)
+        }
+
+    except ClientError as err:
+        error_message = "Couldn't analyze image. " + \
+            err.response['Error']['Message']
+
+        lambda_response = {
+            'statusCode': 400,
+            'body': {
+                "Error": err.response['Error']['Code'],
+                "ErrorMessage": error_message
+            }
+        }
+        logger.error("Error function %s: %s",
+            context.invoked_function_arn, error_message)
+
+    except ValueError as val_error:
+        lambda_response = {
+            'statusCode': 400,
+            'body': {
+                "Error": "ValueError",
+                "ErrorMessage": format(val_error)
+            }
+        }
+        logger.error("Error function %s: %s",
+            context.invoked_function_arn, format(val_error))
+
+    return lambda_response
