225e1de77fc17a4825a3b4f6fce34c71d3320196
Seyfullah Tıkıç <stikic@gmail.com>
2024-05-04 00:18:42 +0300

    Geliştirme yapıldı

--- kelime/kelime_degerlendir.php
+++ kelime/kelime_degerlendir.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="tr">
-
-<head>
-    <title>Kelime Değerlendir</title>
-</head>
-
-<body>
     <?php
     $image = $_POST['image'];
     $image = explode(',', $image);
     $image = $image[1];
-    echo "Resmin ilk 20 karakteri: ".substr($image, 0, 20)."<br/>";
 
     $url = "";
 
     $json = json_encode(['image' => $image]);
-    // echo "content json: ".$json."<hr/>";
-    
+    $json = str_replace(PHP_EOL, '', $json);
+    $json = str_replace(" ", '+', $json);
+
     $options = ['http' => [
         'method' => 'POST',
         'header' => 'Content-type:application/json',
@@ -26,9 +18,4 @@
     $context = stream_context_create($options);
     $response = file_get_contents($url, false, $context);
 
-    echo "<h1>Response:".$response."</h1>";
-
-    ?>
-</body>
-
-</html>
+    echo $response;
